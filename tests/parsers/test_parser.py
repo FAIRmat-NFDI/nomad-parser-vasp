b18a41b849f7dfb4db4cefe4edc5a2f8724889df
Nathan Daelman <ndaelman.physik.hu-berlin.de>
2024-06-30 14:40:12 +0200

    Cruft instantiated

---
+++ tests/parsers/test_parser.py
@@ -0,0 +1,12 @@
+import logging
+
+from nomad.datamodel import EntryArchive
+from nomad_parser_vasp.parsers.myparser import MyParser
+
+
+def test_parse_file():
+    parser = MyParser()
+    archive = EntryArchive()
+    parser.parse('tests/data/example.out', archive, logging.getLogger())
+
+    assert archive.results.material.elements == ['H', 'O']
